c063f3312f5210f2cf0cf4356b29a06b61d83036
rafaelpllopes <rafael.pllopes@gmail.com>
2022-01-24 18:02:23 -0300

    Criando o primeiro componente, entendendo JSX.

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,18 @@
+//Componente
 function HomePage() {
-    return <div>Welcome to Next.js!</div>
+    //JSX
+
+    return (
+        <div>
+            <h1>Boas vindas de volta!</h1>
+            <h2>Olá Alura Matrix</h2>
+            <style jsx>{`
+                h1 {
+                    color: red;
+                }
+                `}</style>
+        </div>
+    )
 }
 
 export default HomePage
